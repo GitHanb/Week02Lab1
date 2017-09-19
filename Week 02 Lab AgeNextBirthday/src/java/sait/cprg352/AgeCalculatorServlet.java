565b7d0faf893be23bf1e176070a95116ae1568b
hanzh <hanzh@192.168.1.144>
2017-09-18 21:13:47 -0600

    Week 02 Lab 1
    Create initial files

---
+++ Week 02 Lab AgeNextBirthday/src/java/sait/cprg352/AgeCalculatorServlet.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sait.cprg352;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 636334
+ */
+public class AgeCalculatorServlet extends HttpServlet 
+{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        // get the parameters from the URL
+        String age = request.getParameter("age");
+
+        
+        // set the JSP request variables to the values first name and last name
+        request.setAttribute("ageNext", (age+1));
+        
+        if (age == null || age.trim().isEmpty()) 
+        {
+            request.setAttribute("errorMessage", "Please enter a number");
+
+            getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").forward(request, response);
+            return;
+        }
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+}
---
+++ Week 02 Lab AgeNextBirthday/web/WEB-INF/ageCalculator.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : ageCalculator
+    Created on : 18-Sep-2017, 3:54:03 PM
+    Author     : 636334
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    <body>
+        <h1>Age next birthday</h1>
+        <div>
+            <form action="ageCalculator" method="get">
+                Enter your current age: <input type="text" name="age" value="${age}">
+                <input type="submit" value="Age next birthday">  
+            </form>    
+        </div>
+         ${ageNext};
+        // ${errorMessage}
+    </body>
+</html>
